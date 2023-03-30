9f3b7a889df3f071641f68e015e4863738ba1c71
Tobias <tobias.riemenschneider@gmx.net>
2023-03-30 21:23:06 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 3001;
   console.log("listening on port ", port)
+  const corsOptions = {
+    "origin": "*"
+  }
+  app.enableCors(corsOptions);
   await app.listen(port);
 }
 bootstrap();
